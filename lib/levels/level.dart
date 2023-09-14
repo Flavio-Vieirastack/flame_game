eb3c7d0565cf8f5825dbeef4f3ed53924b66b402
Flavio Vieira <flavioemerson1@outlook.com>
2023-09-14 13:23:40 -0300

    adicionado mapa

---
+++ lib/levels/level.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'package:flame_tiled/flame_tiled.dart';
+import 'package:flame/components.dart';
+
+class Level extends World {
+  late final TiledComponent level;
+  @override
+  FutureOr<void> onLoad() async {
+    level = await TiledComponent.load(
+      'level_1.tmx',
+      Vector2.all(16),
+    );
+    add(level);
+    return super.onLoad();
+  }
+}
